fb32ca3267ed9129f56efecd6ae49bcff8eaa340
Túlio <lucastuliolima@gmail.com>
2023-07-09 18:48:42 -0300

    determine winner function

--- main.py
+++ main.py
@@ -20,31 +20,27 @@ def computer_selection():
     return action
 
 """Next step is the determine winner function"""
-while True:
-    #user_input = input("Enter a choice (rock[0], paper[1], scissors[2]): ")
-    user_action = int(user_input)
-    possible_actions = ["rock", "paper", "scissors"]
-    computer_action = random.choice(possible_actions)
-    print(f"\nYou chose {user_action}, computer chose {computer_action}. \n")
 
+def determine_winner(user_action, computer_action):
     if user_action == computer_action:
-        print(f"Both players selected {user_action}. It's a draw.")
-    elif user_action == "rock":
-        if computer_action == "scissors":
-            print("Rock smashes scissors! You win!")
+        print(f"Both players selected {user_action.name}. It's a tie!")
+    elif user_action == Action.Rock:
+        if computer_action == Action.Scissors:
+            print("Rock smashes Scissors! You win.")
         else:
-            print("Paper covers rock! You lost!")
-    elif user_action == "paper":
-        if computer_action == "rock":
-            print("Paper covers rock! You win!")
+            print("Paper covers Rock! You lose.")
+    elif user_action == Action.Paper:
+        if computer_action == Action.Rock:
+            print("Paper covers Rock! You lose.")
         else:
-            print("Scissor cuts paper! You lost!")
-    elif user_action == "scissors":
-        if computer_action == "paper":
-            print("Scissors cuts paper! You win!")
+            print("Scissors cuts paper! You lose.")
+    elif user_action == Action.Scissors:
+        if computer_action == Action.Paper:
+            print("Scissors cuts Paper! You win!")
         else:
-            print("Rock smashes scissors! You win!")
-
+            print("Rock smashes Scissors! You lose.")
+"""
     play_again = input("Play again? (y/n): ")
     if play_again.lower() != "y":
         break
+"""
